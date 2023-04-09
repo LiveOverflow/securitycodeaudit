57395ddc45e7e914374152dcd66e24e41af3a287
LiveOverflow <liveoverflow@gmail.com>
2023-04-09 16:37:34 +0200

    deploy gh pages

---
+++ assets/AboutView.11b3d654.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as t,c as a,a as o}from"./index.09b2e381.js";const s={},_={class:"about"},i=o("h1",null,"This is an about page",-1),n=[i];function c(r,d){return t(),a("div",_,n)}var h=e(s,[["render",c]]);export{h as default};
---
+++ assets/Python1.52e4c0b3.js
@@ -0,0 +1,7 @@
+const n={code:`
+def button1():
+    correct()
+
+def button2():
+    wrong()
+`,solve:{button1:()=>!0,button2:()=>!1,button3:t=>t==="test",button4:(t,e)=>t===e}};export{n as default};
